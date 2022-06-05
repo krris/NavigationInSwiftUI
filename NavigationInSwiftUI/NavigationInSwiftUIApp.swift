3c8fce43e59a9c3a0755d7390c104de6f62df2cb
Krzysztof <krrisgit@gmail.com>
2022-06-05 18:24:58 +0200

    Set up project

---
+++ NavigationInSwiftUI/NavigationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationInSwiftUIApp.swift
+//  NavigationInSwiftUI
+//
+//  Created by Krzysztof Werys on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
